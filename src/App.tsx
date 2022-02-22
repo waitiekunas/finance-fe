52543956b00b77aad76825d044068d73a6039f55
Andrius Vaitiekunas <waitiekunas@gmail.com>
2022-02-22 19:44:38 +0200

    Added MUI

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import './App.css';
 import React from 'react';
 import { Route, Routes } from 'react-router';
 
+import Header from './components/Header';
 import Tets1 from './pages/Test1';
 import Tets2 from './pages/Test2';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Tets1 />} />
         <Route path="/test" element={<Tets2 />} />
       </Routes>
+      <div>FOOTER TEST</div>
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material';
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div>
+      HEADER TEST
+      {' '}
+      <Button />
+    </div>
+  );
+}
